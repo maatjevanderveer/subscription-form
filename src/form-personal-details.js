ba86b71b0e8a8de7e1fedcef6f6d17766f862177
Maatje van der Veer <mjvanderveer@gmail.com>
2018-10-02 14:31:14 +0200

    Disables continuation button when fields are not filled in or error's are found

--- src/form-personal-details.js
+++ src/form-personal-details.js
@@ -1,90 +1,90 @@
 import React from 'react';
-import { Field, ErrorMessage } from 'formik';
-import * as Yup from "yup";
+import {Field, ErrorMessage} from 'formik';
+import * as Yup from 'yup';
 import PropTypes from 'prop-types';
 
 export class FormPersonalDetails extends React.Component {
-   render() {
-       return(
-           <React.Fragment>
-               <label>Voornaam:
-                   <Field type="text"
-                          name="firstName"
-                          autoComplete="given-name"
-                          placeholder="Voornaam"
-                          autoFocus={true}/>
-                   <ErrorMessage name="firstName">
-                       {msq => <div>{msq}</div>}
-                   </ErrorMessage>
-               </label>
-               <br />
-               <label>Achternaam:
-                   <Field type="text"
-                          name="lastName"
-                          autoComplete="family-name"
-                          placeholder="Achternaam"/>
-                   <ErrorMessage name="lastName">
-                       {msq => <div>{msq}</div>}
-                   </ErrorMessage>
-               </label>
-               <br />
-               <label>Geboortedatum:
-                   <Field type="numeric"
-                          name="bdayDay"
-                          autoComplete="bday-day"
-                          placeholder="Dag"
-                          maxLength="2"/>
-                   <ErrorMessage name="bdayDay">
-                       {msq => <div>{msq}</div>}
-                   </ErrorMessage>
-                   <Field component="select"
-                          name="bdayMonth"
-                          autoComplete="bday-month"
-                          placeholder="Maand">
-                       <option value="januari">januari</option>
-                       <option value="februari">februari</option>
-                       <option value="maart">maart</option>
-                       <option value="april">april</option>
-                       <option value="mei">mei</option>
-                       <option value="juni">juni</option>
-                       <option value="juli">juli</option>
-                       <option value="augustus">augustus</option>
-                       <option value="september">september</option>
-                       <option value="oktober">oktober</option>
-                       <option value="november">november</option>
-                       <option value="december">december</option>
-                   </Field>
-                   <Field type="numeric"
-                          name="bdayYear"
-                          autoComplete="bday-year"
-                          placeholder="Jaar"
-                          maxLength="4"/>
-                   <ErrorMessage name="bdayYear">
-                       {msq => <div>{msq}</div>}
-                   </ErrorMessage>
-               </label>
-               <br />
-               <label>Geslacht - optioneel:
-                   <Field type="radio"
-                          name="gender"
-                          value="vrouw"
-                          id="vrouw"/>
-                   <label htmlFor="vrouw">Vrouw</label>
-                   <Field type="radio"
-                          name="gender"
-                          value="man"
-                          id="man"/>
-                   <label htmlFor="man">Man</label>
-                   <Field type="radio"
-                          name="gender"
-                          value="anders"
-                          id="anders" />
-                   <label htmlFor="anders">Anders</label>
-               </label>
-               <br />
-           </React.Fragment>
-       )
-   }
+    render() {
+        return (
+            <React.Fragment>
+                <label>Voornaam:
+                    <Field type="text"
+                           name="firstName"
+                           autoComplete="given-name"
+                           placeholder="Voornaam"
+                           autoFocus={true}/>
+                    <ErrorMessage name="firstName">
+                        {msq => <div>{msq}</div>}
+                    </ErrorMessage>
+                </label>
+                <br/>
+                <label>Achternaam:
+                    <Field type="text"
+                           name="lastName"
+                           autoComplete="family-name"
+                           placeholder="Achternaam"/>
+                    <ErrorMessage name="lastName">
+                        {msq => <div>{msq}</div>}
+                    </ErrorMessage>
+                </label>
+                <br/>
+                <label>Geboortedatum:
+                    <Field type="numeric"
+                           name="bdayDay"
+                           autoComplete="bday-day"
+                           placeholder="Dag"
+                           maxLength="2"/>
+                    <ErrorMessage name="bdayDay">
+                        {msq => <div>{msq}</div>}
+                    </ErrorMessage>
+                    <Field component="select"
+                           name="bdayMonth"
+                           autoComplete="bday-month"
+                           placeholder="Maand">
+                        <option value="januari">januari</option>
+                        <option value="februari">februari</option>
+                        <option value="maart">maart</option>
+                        <option value="april">april</option>
+                        <option value="mei">mei</option>
+                        <option value="juni">juni</option>
+                        <option value="juli">juli</option>
+                        <option value="augustus">augustus</option>
+                        <option value="september">september</option>
+                        <option value="oktober">oktober</option>
+                        <option value="november">november</option>
+                        <option value="december">december</option>
+                    </Field>
+                    <Field type="numeric"
+                           name="bdayYear"
+                           autoComplete="bday-year"
+                           placeholder="Jaar"
+                           maxLength="4"/>
+                    <ErrorMessage name="bdayYear">
+                        {msq => <div>{msq}</div>}
+                    </ErrorMessage>
+                </label>
+                <br/>
+                <label>Geslacht - optioneel:
+                    <Field type="radio"
+                           name="gender"
+                           value="vrouw"
+                           id="vrouw"/>
+                    <label htmlFor="vrouw">Vrouw</label>
+                    <Field type="radio"
+                           name="gender"
+                           value="man"
+                           id="man"/>
+                    <label htmlFor="man">Man</label>
+                    <Field type="radio"
+                           name="gender"
+                           value="anders"
+                           id="anders"/>
+                    <label htmlFor="anders">Anders</label>
+                </label>
+                <br/>
+            </React.Fragment>
+        );
+    }
 }
 
 FormPersonalDetails.propTypes = {
--- src/index.js
+++ src/index.js
@@ -1,98 +1,116 @@
 import React from 'react';
-import { Formik, Form, ErrorMessage, Field } from 'formik';
+import {Formik, Form, ErrorMessage, Field} from 'formik';
 import './styles.css';
-import { FormPersonalDetails } from './form-personal-details.js';
-import { FormShippingAddress } from './form-shipping-address.js';
-import { FormNewAccount } from './form-new-account.js';
+import {FormPersonalDetails} from './form-personal-details.js';
+import {FormShippingAddress} from './form-shipping-address.js';
+import {FormNewAccount} from './form-new-account.js';
 import * as Yup from 'yup';
+import _ from 'underscore';
 
-const pages = [<FormPersonalDetails />, <FormShippingAddress />, <FormNewAccount />];
+const pages = [<FormPersonalDetails/>, <FormShippingAddress/>, <FormNewAccount/>];
+
+
+const validationSchema = Yup.object().shape({
+    firstName: Yup.string()
+        .min(2, 'Te kort')
+        .max(70, 'Te lang')
+        .required('Verplicht'),
+    lastName: Yup.string()
+        .min(2, 'Te kort')
+        .max(100, 'Te lang')
+        .required('Verplicht'),
+    bdayDay: Yup.number()
+        .integer('Vul een getal in')
+        .positive('Ongeldig')
+        .lessThan(32, 'Ongeldig')
+        .required('Verplicht'),
+    bdayYear: Yup.number()
+        .integer('Vul een getal in')
+        .positive('Ongeldig')
+        .max(2018, 'Ben je in de toekomst geboren?')
+        .min(1900, 'Ongeldig')
+        .required('Verplicht'),
+    postalCode: Yup.string()
+        .matches(/^[1-9][0-9]{3}[\s]?[A-Za-z]{2}$/i, 'Bijv. 1234AB')
+        .required('Verplicht'),
+    houseNumber: Yup.number()
+        .integer('Vul een getal in')
+        .positive('Ongeldig')
+        .required('Verplicht'),
+    streetAddress: Yup.string(),
+    city: Yup.string(),
+    email: Yup.string()
+        .email('Bijv. email@voorbeeld.nl'),
+});
+
+const fieldsPerPage = [
+    ['firstName', 'lastName', 'bdayDay', 'bdayYear'],
+    ['postalCode', 'houseNumber', 'streetAddress', 'city'],
+    ['email', 'password']
+];
 
 class SubscriptionForm extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             page: 0,
+        };
+    };
+
+    hasErrors = (values, errors) => {
+        const pageErrors = _.pick(errors, fieldsPerPage[this.state.page]);
+        if (Object.keys(pageErrors).length !== 0) {
+            return true;
         }
+        return false;
     };
 
     render() {
-        const validationSchema = Yup.object().shape({
-            firstName: Yup.string()
-                .min(2, "Te kort")
-                .max(70, "Te lang")
-                .required("Verplicht"),
-            lastName: Yup.string()
-                .min(2, "Te kort")
-                .max(100, "Te lang")
-                .required("Verplicht"),
-            bdayDay: Yup.number()
-                .integer("Vul een getal in")
-                .positive("Ongeldig")
-                .lessThan(32, "Ongeldig")
-                .required("verplicht"),
-            bdayYear: Yup.number()
-                .integer("Vul een getal in")
-                .positive("Ongeldig")
-                .max(2018, "Ben je in de toekomst geboren?")
-                .min(1900, "Ongeldig")
-                .required("Verplicht"),
-            postalCode: Yup.string()
-                .matches(/^[1-9][0-9]{3}[\s]?[A-Za-z]{2}$/i, "Bijv. 1234AB")
-                .required(),
-            houseNumber: Yup.number()
-                .integer("Vul een getal in")
-                .positive("Ongeldig")
-                .required(),
-            streetAddress: Yup.string()
-                .required(),
-            city: Yup.string()
-                .required(),
-            email: Yup.string()
-                .email("Bijv. email@voorbeeld.nl")
-                .required()
-        });
 
         return (
-            <div>
-                <h1>Cineville</h1>
-                <Formik initialValues={initialValues}
-                        validationSchema={validationSchema}
-                        onSubmit={(values) => console.log("values:", values)}>
-
+            <Formik initialValues={initialValues}
+                    validate={values => {
+                        console.log(values);
+                    }}
+                    validationSchema={validationSchema}
+                    onSubmit={(values) => console.log('values:', values)}
+                    render={({errors, values}) => (
                         <Form>
                             {pages[this.state.page]}
-                            {this.state.page === 0 ? null : <button type="button" onClick={this.previousPage}>Vorige</button>}
-                            {this.state.page === pages.length -1 ? <button type="submit">Naar betalen</button> : <button type="button" onClick={this.nextPage}>Verder</button>}
+                            {this.state.page === 0 ? null :
+                                <button type="button" onClick={this.previousPage}>Vorige</button>}
+                            {this.state.page === pages.length - 1 ? <button type="submit">Naar betalen</button> :
+                                <button type="button" onClick={this.nextPage}
+                                        disabled={this.hasErrors(values, errors)}>Verder</button>}
                         </Form>
-
-                </Formik>
-            </div>
-        )
+                    )}
+            >
+            </Formik>
+        );
     };
 
     previousPage = () => {
-        this.setState(state => ({ page: state.page -1}))
+        this.setState(state => ({page: state.page - 1}));
     };
 
     nextPage = () => {
-        this.setState(state => ({ page: state.page + 1 }));
-    }
+        this.setState(state => ({page: state.page + 1}));
+    };
 }
 
 const initialValues = {
-    firstName: "",
-    lastName: "",
-    bdayDay: "",
-    bdayMonth: "",
-    bdayYear: "",
-    postalCode: "",
-    houseNumber: "",
-    addition: "",
-    streetAddress: "",
-    city: "",
-    email: "",
-    password: ""
+    firstName: '',
+    lastName: '',
+    bdayDay: '',
+    bdayMonth: '',
+    bdayYear: '',
+    postalCode: '',
+    houseNumber: '',
+    addition: '',
+    streetAddress: '',
+    city: '',
+    email: '',
+    password: ''
 };
 
-export default SubscriptionForm
+export default SubscriptionForm;
